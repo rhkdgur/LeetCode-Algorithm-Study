f5988afb600322bec42f35a797a9f0f3e3ce9180
광돌이 <67618667+rhkdgur@users.noreply.github.com>
2024-07-09 16:37:22 +0900

    Time: 0 ms (100.00%) | Memory: 45.6 MB (26.90%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -1,32 +1,45 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
 
+        // int len = flowerbed.length;
+        // int[] temp = flowerbed;
+        // int cnt = 0;
+        // int cnt2 = 0;
+        // for(int i = 0; i<len; i++) {
+        //     if(temp[i] == 1){
+        //         cnt2++;
+        //     }
+        //     if(i == 0) {
+        //         if(flowerbed[i] != 1){
+        //             flowerbed[i] = 1;
+        //         }
+        //         cnt++;
+        //     }else {
+        //         if(flowerbed[i-1] != 1){
+        //             flowerbed[i] = 1;
+        //             cnt++;
+        //         }
+        //     }
+        // }
+
+        // if((cnt - cnt2) >= n) {
+        //     return true;
+        // }else {
+        //     return false;
+        // }
+
         int len = flowerbed.length;
-        int[] temp = flowerbed;
-        int cnt = 0;
-        int cnt2 = 0;
-        for(int i = 0; i<len; i++) {
-            if(temp[i] == 1){
-                cnt2++;
-            }
-            if(i == 0) {
-                if(flowerbed[i] != 1){
-                    flowerbed[i] = 1;
-                }
-                cnt++;
-            }else {
-                if(flowerbed[i-1] != 1){
-                    flowerbed[i] = 1;
-                    cnt++;
+        for(int i = 0; i<len; i=i+2){
+            if(flowerbed[i] == 0) {
+                if(i == len - 1 || flowerbed[i] == flowerbed[i+1]) {
+                    n--;
+                }else {
+                    i++;
                 }
             }
         }
 
-        if((cnt - cnt2) >= n) {
-            return true;
-        }else {
-            return false;
-        }
+        return n <= 0;
         
     }
 }
