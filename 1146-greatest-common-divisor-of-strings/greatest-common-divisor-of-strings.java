5d70e0754850b2d299d7c5dbb5ec6d8f8078211f
광돌이 <67618667+rhkdgur@users.noreply.github.com>
2024-07-10 15:35:45 +0900

    Time: 1 ms (74.79%) | Memory: 41.9 MB (79.54%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -1,23 +1,22 @@
 class Solution {
-
-
-
     public String gcdOfStrings(String str1, String str2) {
-        
-        StringBuffer s = new StringBuffer();
 
-        if(!(str1+str2).equals(str2+str1)){
-            return "";
-        }else {
-            int i = str1.length();
-            int j = str2.length();
-            while(j != 0){
-                int temp = j;
-                j = i % j;
-                i = temp;
-            }
-            return str1.substring(0,i);
-        }
+        StringBuilder str = new StringBuilder();
         
+        if((str1+str2).equals(str2+str1)) {
+            int len1 = str1.length();
+            int len2 = str2.length();
+
+            //최대 공약수를 구하라
+            //str1
+            while(len2 != 0) {
+                int temp = len2;
+                len2 = len1 % len2;
+                len1 = temp;
+            }
+            str.append(str1.substring(0,len1));
+        }
+
+        return str.toString();
     }
 }
