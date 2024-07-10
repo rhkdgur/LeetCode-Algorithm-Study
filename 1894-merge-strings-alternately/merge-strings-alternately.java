e64e22927c1177f9a787384c7fb86794b01b8aa2
광돌이 <67618667+rhkdgur@users.noreply.github.com>
2024-07-10 15:00:05 +0900

    Time: 2 ms (33.12%) | Memory: 42.1 MB (31.06%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,19 +1,20 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        String[] w1 = word1.split("");
-        String[] w2 = word2.split("");
+        
         StringBuffer str = new StringBuffer();
+        int len1 = word1.length();
+        int len2 = word2.length();
 
-        for(int i = 0; i<w1.length; i++){
-            str.append(w1[i]);
-            if(i < w2.length){
-                str.append(w2[i]);
+        for(int i = 0; i<len1; i++) {
+            str.append(word1.charAt(i)+"");
+            if(i < len2) {
+                str.append(word2.charAt(i)+"");
             }
-        } 
+        }
 
-        if(w2.length > w1.length) {
-            for(int i = w1.length; i<w2.length; i++){
-                 str.append(w2[i]);
+        if(len1 < len2) {
+            for(int i = len1; i<len2; i++) {
+                str.append(word2.charAt(i)+"");
             }
         }
 
