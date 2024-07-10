cdf869446552af562ee5ed5cc8fc9413414b112c
광돌이 <67618667+rhkdgur@users.noreply.github.com>
2024-07-10 14:25:07 +0900

    Time: 2 ms (32.26%) | Memory: 44.6 MB (8.13%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -22,22 +22,22 @@ class Solution {
 		// 	str.append(cnt);
 		// }
 
-        int cnt = 0;
+        int cnt = 1;
 		for(int i = 0; i<len; i++) {
 			if(i < len - 1) {
 				if(chars[i] == chars[i+1]) {
 					cnt++;
 				}else {
 					str.append(chars[i]);
-					if(cnt > 0) {
-						str.append(cnt+1);
+					if(cnt > 1) {
+						str.append(cnt);
 					}
-					cnt = 0;
+					cnt = 1;
 				}
 			}else {
                 str.append(chars[i]);
-                if(cnt > 0){
-                    str.append(cnt+1);
+                if(cnt > 1){
+                    str.append(cnt);
                 }
             }
 		}
