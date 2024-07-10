2ba45923525f395cdaa96dc320ea5287c1e344bf
광돌이 <67618667+rhkdgur@users.noreply.github.com>
2024-07-10 14:22:09 +0900

    Time: 2 ms (32.26%) | Memory: 44.1 MB (31.71%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -3,24 +3,44 @@ class Solution {
         int len = chars.length;
 		StringBuffer str = new StringBuffer();
 		
-		int cnt = 1;
-		for(int i = 1; i<len; i++) {
-			if(chars[i] == chars[i-1]) {
-				cnt++;
-			}else {
-				if(cnt > 1) {
-					str.append(chars[i-1]).append(cnt);
+		// int cnt = 1;
+		// for(int i = 1; i<len; i++) {
+		// 	if(chars[i] == chars[i-1]) {
+		// 		cnt++;
+		// 	}else {
+		// 		if(cnt > 1) {
+		// 			str.append(chars[i-1]).append(cnt);
+		// 		}else {
+		// 			str.append(chars[i-1]);
+		// 		}
+		// 		cnt = 1; 
+		// 	}
+		// }
+		
+		// str.append(chars[len-1]);
+		// if(cnt > 1) {
+		// 	str.append(cnt);
+		// }
+
+        int cnt = 0;
+		for(int i = 0; i<len; i++) {
+			if(i < len - 1) {
+				if(chars[i] == chars[i+1]) {
+					cnt++;
 				}else {
-					str.append(chars[i-1]);
+					str.append(chars[i]);
+					if(cnt > 0) {
+						str.append(cnt+1);
+					}
+					cnt = 0;
 				}
-				cnt = 1; 
 			}
 		}
-		
-		str.append(chars[len-1]);
-		if(cnt > 1) {
-			str.append(cnt);
-		}
+
+        str.append(chars[len-1]);
+        if(cnt > 0){
+            str.append(cnt+1);
+        }
 		
         for(int i = 0; i<str.toString().length(); i++) {
             chars[i] = str.toString().charAt(i);
