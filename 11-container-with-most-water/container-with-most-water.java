c525b622ac78ee60016f12b7ac40c84108bbc4e6
광돌이 <67618667+rhkdgur@users.noreply.github.com>
2024-07-11 16:00:09 +0900

    Time: 2 ms (98.68%) | Memory: 57.7 MB (52.85%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,23 +1,30 @@
 class Solution {
     public int maxArea(int[] height) {
         
-       int len = height.length;
+        int len = height.length;
 		int left = 0;
 		int right = len - 1;
 		int max = 0;
-		int hmin = 0;
+
+        // 양쪽 point로부터 서로 가까워 지는 것이기에 교차되는 순간 while 문 탈출
         while(left < right) {
-        	// 높이 최소값
-        	hmin = height[left] < height[right] ? height[left] : height[right];
-        	// 넓이 최대값
-        	max = max < (right - left) * hmin ? (right - left) * hmin : max;  
         	
-        	if(height[left] <= hmin) {
-        		left++;
-        	}
-        	if(height[right] <= hmin) {
-        		right--;
-        	}
+            // x 좌표 양 측 point 길이
+        	int w = right - left;
+            // y 높이를 의미  , 두 point 중 작은 값을 선택 ( 물 통이 기울어지면 안된닥고 되어 있음)
+        	int h = height[left] > height[right] ? height[right] : height[left];
+        	
+            // 직사가형 넓이 구하는식 적용
+        	int b = w * h;
+            // 직사각형 들의 최대값이 뭔지 체크
+        	max = max < b ? b : max;
+        	
+            while(left < right && height[left] <= h) {
+                left++;
+            }
+            while(left < right && height[right] <= h){
+                right--;
+            }
         }
         
         return max;
