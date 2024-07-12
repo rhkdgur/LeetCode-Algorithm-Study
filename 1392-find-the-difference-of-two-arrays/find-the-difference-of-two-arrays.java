12fe90a907ec6bd18f64d2d62fdc9c783b0dfd70
광돌이 <67618667+rhkdgur@users.noreply.github.com>
2024-07-12 11:02:23 +0900

    Time: 13 ms (35.49%) | Memory: 45.7 MB (8.10%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -1,8 +1,5 @@
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-        Map<Integer,Integer> map = new LinkedHashMap<>();
-        Map<Integer,Integer> map2 = new LinkedHashMap<>();
-
         List<List<Integer>> result = new ArrayList<>();
         HashSet<Integer> s1 = new HashSet<>();
         HashSet<Integer> s2 = new HashSet<>();
@@ -15,24 +12,13 @@ class Solution {
             s2.add(nums2[i]);
         }
 
+        HashSet<Integer> temp = new HashSet<>(s1);
 
+        s1.removeAll(s2);
+        s2.removeAll(temp);
 
         List<Integer> l1 = new ArrayList<>(s1);
         List<Integer> l2 = new ArrayList<>(s2);
-
-        for(int i = 0; i<l1.size(); i++) {
-            map.put(l1.get(i),l1.get(i));
-        }
-
-        for(int i = 0; i<l2.size(); i++) {
-            if(map.get(l2.get(i)) != null){
-                s1.remove(l2.get(i));
-                s2.remove(l2.get(i));
-            }
-        }
-
-        l1 = new ArrayList<>(s1);
-        l2 = new ArrayList<>(s2);
 
         result.add(l1);
         result.add(l2);
