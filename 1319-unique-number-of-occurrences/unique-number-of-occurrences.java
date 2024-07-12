b29cf8457faaaea5e7dc0bf15a272d6fb8295ccf
광돌이 <67618667+rhkdgur@users.noreply.github.com>
2024-07-12 16:47:51 +0900

    Time: 2 ms (96.56%) | Memory: 41.7 MB (58.85%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -3,11 +3,7 @@ class Solution {
         Map<Integer,Integer> map = new HashMap<>();
         Set<Integer> s = new HashSet<>();
         for(int i = 0; i<arr.length; i++) {
-            if(map.get(arr[i]) == null) {
-                map.put(arr[i],1);
-            }else {
-                map.put(arr[i],map.get(arr[i])+1);
-            }
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
         }
 
         int len = 0;
