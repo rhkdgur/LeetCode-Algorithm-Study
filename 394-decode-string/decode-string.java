1fe3e348d9d041713e9f102187ba180e2801aebe
광돌이 <67618667+rhkdgur@users.noreply.github.com>
2024-07-16 01:03:30 +0900

    Time: 1 ms (79.09%) | Memory: 41.6 MB (28.85%) - LeetSync

---
+++ 394-decode-string/decode-string.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String decodeString(String s) {
+        
+        Stack<Integer> stacks = new Stack<>();
+        Stack<StringBuffer> stacks2 = new Stack<>();
+        StringBuffer str = new StringBuffer();
+
+        int n = 0;
+        for(char ch : s.toCharArray()) {
+            // 숫자가 나오는 경우
+            if(Character.isDigit(ch)) {
+                n = n*10 + (ch - '0');
+            }else if(ch == '[') {
+                stacks.push(n);
+                n = 0;
+                stacks2.push(str);
+                str = new StringBuffer();
+            }else if(ch == ']') {
+                int num = stacks.pop();
+                StringBuffer temp = str;
+                str = stacks2.pop();
+                while(num-- > 0) {
+                    str.append(temp);
+                }
+            }else {
+                str.append(ch);
+            }
+        }
+
+        return str.toString();
+
+    }
+}
