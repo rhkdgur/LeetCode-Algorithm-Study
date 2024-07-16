3f324852167273c60d6ef2253656ec7476e7ef8a
광돌이 <67618667+rhkdgur@users.noreply.github.com>
2024-07-17 07:18:16 +0900

    Time: 20 ms (88.77%) | Memory: 54.5 MB (26.16%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.java
+++ 969-number-of-recent-calls/number-of-recent-calls.java
@@ -1,6 +1,6 @@
 class RecentCounter {
 
-    Queue<Integer> que;
+    Queue<Integer> que = null;
 
     public RecentCounter() {
         que = new LinkedList<>();
@@ -8,11 +8,11 @@ class RecentCounter {
     
     public int ping(int t) {
         que.add(t);
-        //제일 마지막 시간 기준으로 3000ms 범위 내에 존재해야함
-        //그래서 제일 처음에 들어온 값부터 시작하여 범위가 잘리게됨
-        while(que.peek() < t-3000) {
+
+        while(que.peek() < t - 3000) {
             que.poll();
         }
+
         return que.size();
     }
 }
