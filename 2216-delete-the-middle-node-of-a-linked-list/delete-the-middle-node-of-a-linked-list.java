2562b9ded565178154cb503ca9b84f7c9fb2f771
광돌이 <67618667+rhkdgur@users.noreply.github.com>
2024-07-22 16:39:58 +0900

    Time: 3 ms (99.66%) | Memory: 64 MB (45.95%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
@@ -10,31 +10,21 @@
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
+
+        if(head == null) return null;
         
-        if(head == null || head.next == null) return null;
+        ListNode temp = new ListNode(0);
+        temp.next = head;
+        ListNode first = temp;
+        ListNode second = head;
 
-        //n/2의 값을 찾기 위해서는 
-        //finished 하나씩 넘어가느 것과 search의 두단계식 넘어가는 노드사이의 차이를 구하면 됨
-        ListNode finished = head;
-        ListNode search = head;
-
-        //node의 중간 지점 찾기 finished가 그 위치를 알려
-        while(search != null && search.next != null) {
-            finished = finished.next;
-            search = search.next.next;
+        while(second != null && second.next != null) {
+            first = first.next;
+            second = second.next.next;
         }
 
-        search = head;
+        first.next = first.next.next;
 
-        while(true) {
-            if(search.next == finished) {
-                search.next = search.next.next;
-                break;
-            }
-            search = search.next;
-        }
-
-        return head;
-
+        return temp.next;
     }
 }
