ce01f4431655b3f964c9ace271da745fbb21541a
광돌이 <67618667+rhkdgur@users.noreply.github.com>
2024-07-22 18:01:36 +0900

    Time: 0 ms (100.00%) | Memory: 42.4 MB (29.67%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -15,10 +15,10 @@ class Solution {
         
         //node의 앞 요소와 뒷 요소의 위치를 바꿔주는 작업을 지속적으로 한다.
         while(head != null) {
-            ListNode temp = head.next;
-            head.next = node;
-            node = head;
-            head = temp;
+            ListNode temp = head;
+            head = head.next;
+            temp.next = node;
+            node = temp;
         }
 
         return node;
