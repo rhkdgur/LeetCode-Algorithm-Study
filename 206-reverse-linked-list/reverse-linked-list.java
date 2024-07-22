7d06e9c72b299f4ba73e0b50f29244e2b16e5435
광돌이 <67618667+rhkdgur@users.noreply.github.com>
2024-07-22 17:42:13 +0900

    Time: 0 ms (100.00%) | Memory: 42.8 MB (8.42%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        
+        ListNode node = null;
+        
+        //node의 앞 요소와 뒷 요소의 위치를 바꿔주는 작업을 지속적으로 한다.
+        while(head != null) {
+            ListNode temp = head.next;
+            head.next = node;
+            node = head;
+            head = temp;
+        }
+
+        return node;
+    }
+}
