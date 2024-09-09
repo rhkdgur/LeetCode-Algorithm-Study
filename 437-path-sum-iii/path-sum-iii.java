bd9eef80ecb580eb756dcca4c01bcddb6659557b
광돌이 <67618667+rhkdgur@users.noreply.github.com>
2024-09-09 17:54:29 +0900

    Time: 19 ms (26.93%) | Memory: 42.8 MB (93.13%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.java
+++ 437-path-sum-iii/path-sum-iii.java
@@ -15,26 +15,24 @@
  */
 class Solution {
 
-    int total = 0;
+    int cnt = 0;
     public int pathSum(TreeNode root, int targetSum) {
         if(root == null) return 0;
-
-        targetCnt(root, targetSum,0);
+        targetCnt(root,targetSum,0);
         pathSum(root.left,targetSum);
         pathSum(root.right,targetSum);
-
-        return  total;
+        return cnt;
     }
 
-    public void targetCnt(TreeNode root, int targetSum, long count) {
+    public void targetCnt(TreeNode root, int targetSum, long sum) {
+        
         if(root == null) return;
 
-        long cnt = count + root.val;
+        long s = sum + root.val;
+        if(s == targetSum) cnt++;
+        
+        targetCnt(root.left, targetSum, s);
+        targetCnt(root.right, targetSum, s);
 
-        if(targetSum == cnt)  total++;
-
-        targetCnt(root.left,targetSum,cnt);
-        targetCnt(root.right,targetSum,cnt);
-    
     }
 }
