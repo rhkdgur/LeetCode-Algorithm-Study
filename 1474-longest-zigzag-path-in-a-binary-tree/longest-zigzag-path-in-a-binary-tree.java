2cb20bb93004d88568cf3e02ed90912a2ecfb7a1
광돌이 <67618667+rhkdgur@users.noreply.github.com>
2024-11-01 18:39:18 +0900

    Time: 5 ms (63.76%) | Memory: 55.2 MB (37.78%) - LeetSync

--- 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.java
+++ 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.java
@@ -16,31 +16,31 @@
 class Solution {
 
     int max = 0;
-
     public int longestZigZag(TreeNode root) {
         if(root == null)
             return 0;
-
-        maxZigzag(root.left, true , 1);
-        maxZigzag(root.right, false , 1);
-
+        
+        checkZigZag(root.left, true, 1);
+        checkZigZag(root.right, false, 1);
         return max;
     }
 
-    public void maxZigzag(TreeNode root , boolean flag , int dept) {
+    public void checkZigZag(TreeNode root, boolean flag, int dept) {
+
         if(root == null)
             return ;
 
-        max = Math.max(max, dept);
+        max = Math.max(dept, max);
 
-        //flag : true -> left, flag : false -> right
         if(flag) {
-            maxZigzag(root.left, true, 1);
-            maxZigzag(root.right, false, dept + 1);
+            checkZigZag(root.left,true, 1);
+            checkZigZag(root.right,false, dept + 1);
         }else {
-            maxZigzag(root.left, true, dept + 1);
-            maxZigzag(root.right, false, 1);
+            checkZigZag(root.left,true, dept+1);
+            checkZigZag(root.right, false, 1);
         }
 
     }
+
+
 }
