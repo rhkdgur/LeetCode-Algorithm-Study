ea06003f542480ea34074c00a84442aacdf7e170
광돌이 <67618667+rhkdgur@users.noreply.github.com>
2024-11-06 17:33:09 +0900

    Time: 0 ms (100.00%) | Memory: 46 MB (47.76%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -17,32 +17,32 @@ class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
         if(root == null)
             return root;
-
-        if(root.val < key)
+            
+        if(root.val < key) {
             root.right = deleteNode(root.right, key);
-        else if(root.val > key) {
+        } else if(root.val > key) {
             root.left = deleteNode(root.left, key);
         }else {
             if(root.left == null && root.right == null)
                 return null;
 
-            if(root.left == null)
+            if(root.left == null) {
                 return root.right;
-            if(root.right == null)
+            } 
+            if(root.right == null) {
                 return root.left;
-            
-            TreeNode rn = find(root.right);
-            root.val = rn.val;
-            root.right = deleteNode(root.right,rn.val);
+            }
+
+            TreeNode r = linkedTree(root.right);
+            root.val = r.val;
+            root.right = deleteNode(root.right,r.val);
         }
 
         return root;
     }
 
-    //최소값으로 연결하는 작업
-    //이진트리탐색의 특성은 노드 왼쪽에 작은 값을 배치한다.
-    public TreeNode find(TreeNode node) {
-        while(node.left != null){
+    public TreeNode linkedTree(TreeNode node) {
+        while(node.left != null) {
             node = node.left;
         }
         return node;
